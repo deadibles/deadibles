f1ac2fd9da01c1facfda020673127a15addba924
Kari Mashurn <kmashburn@student.fullsail.edu>
2021-04-07 18:57:42 -0400

    getting logo and main working

--- website/src/App.js
+++ website/src/App.js
@@ -1,16 +1,26 @@
 import  LogoSqColor  from './Images/LogoSqColor.png';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import ThisMain from './components/main';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <img src={LogoSqColor} className="App-logo" alt="logo" />
-
+    <Router style={s.bg}>
+      <div style={s.head}>
+        <img src={LogoSqColor} height={300} className='App-logo' alt='logo' />
       </div>
+      <ThisMain />
     </Router>
   );
 }
 
 export default App;
+const s = {
+  head: {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: '#375a2a',
+  },
+  bg: {
+    backgroundColor: '#375a2a',
+  },
+};
---
+++ website/src/components/main.js
@@ -0,0 +1,17 @@
+
+
+function Main() {
+  return (
+      <div style={s.head}>
+      <h1>Products Currently Available</h1>
+    </div>
+  );
+}
+
+export default Main;
+
+const s = {
+    head: {
+        display: 'block',
+    }
+}

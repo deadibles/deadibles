a901c030e47188064efd8787135f91c9f8ab3ac1
Kari Mashurn <kmashburn@student.fullsail.edu>
2021-06-21 19:05:27 -0400

    created items model

---
+++ fullStack/models/Item.js
@@ -0,0 +1,31 @@
+const mongoose = require( 'mongooose' );
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema( {
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+} );
+
+module.exports = Item = mongoose.model( 'item', ItemSchema );

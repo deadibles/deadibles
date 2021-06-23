184792a055c63b0a86be17580bcd18c3b644ca78
Kari Mashurn <kmashburn@student.fullsail.edu>
2021-06-23 18:26:28 -0400

    created react app, created store, types, and auth actions

---
+++ fullStack/client/src/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleaware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const middleWare = [ thunk ];
+
+const store = createStore( rootReducer, initialState, compose(
+    applyMiddleaware( ...middleWare ),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+) );
+
+export default store;
